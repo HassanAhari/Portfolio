e3114b50179c90de6b54d2b09a673614b1e9789a
hassan <hsoleimaneahary@gmail.com>
2023-09-12 19:32:00 +0330

    init my portfolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			primary: '#ff4b57',
+			natural: '#23253a',
+			content: '#f7fafd',
+			sub: '#aaa',
+			card: "#2b2d41"
+		},
+	},
+	plugins: [],
+}
